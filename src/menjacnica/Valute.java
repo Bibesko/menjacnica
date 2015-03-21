64c694262fedd70878fa892ead842f206da792eb
Bibesko <bibesko94@gmail.com>
2015-03-21 17:14:32 +0100

    Napravljen paket menjacnica i dve klase u njemu (Kurs i Valute) u klasamu su napravljeni odredjeni atributi za te klase.

---
+++ src/menjacnica/Valute.java
@@ -0,0 +1,22 @@
+package menjacnica;
+
+public class Valute {
+
+	private String nazivValute;
+	private String skraceniNazivValute;
+	
+	
+	public String getNazivValute() {
+		return nazivValute;
+	}
+	public void setNazivValute(String nazivValute) {
+		this.nazivValute = nazivValute;
+	}
+	public String getSkraceniNazivValute() {
+		return skraceniNazivValute;
+	}
+	public void setSkraceniNazivValute(String skraceniNazivValute) {
+		this.skraceniNazivValute = skraceniNazivValute;
+	}
+			
+}
