b0c795e378c1cd5a1957048b9b7c20b589939417
Bibesko <bibesko94@gmail.com>
2015-03-21 17:18:59 +0100

    U sve postojece klase i za sve atribute u njima dodate su metode equals, hashCode i toString.

--- src/menjacnica/Valute.java
+++ src/menjacnica/Valute.java
@@ -18,5 +18,46 @@ public class Valute {
 	public void setSkraceniNazivValute(String skraceniNazivValute) {
 		this.skraceniNazivValute = skraceniNazivValute;
 	}
+	
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((nazivValute == null) ? 0 : nazivValute.hashCode());
+		result = prime
+				* result
+				+ ((skraceniNazivValute == null) ? 0 : skraceniNazivValute
+						.hashCode());
+		return result;
+	}
+	
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valute other = (Valute) obj;
+		if (nazivValute == null) {
+			if (other.nazivValute != null)
+				return false;
+		} else if (!nazivValute.equals(other.nazivValute))
+			return false;
+		if (skraceniNazivValute == null) {
+			if (other.skraceniNazivValute != null)
+				return false;
+		} else if (!skraceniNazivValute.equals(other.skraceniNazivValute))
+			return false;
+		return true;
+	}
+	
+
+	public String toString() {
+		return "Valute [nazivValute=" + nazivValute + ", skraceniNazivValute="
+				+ skraceniNazivValute + "]";
+	}
 			
 }
