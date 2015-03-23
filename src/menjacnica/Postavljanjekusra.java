505fd43447bdc8752d8df95b56c69add269f5aa1
Bibesko <bibesko94@gmail.com>
2015-03-23 21:24:06 +0100

    Ubacene metode.

--- src/menjacnica/Postavljanjekusra.java
+++ src/menjacnica/Postavljanjekusra.java
@@ -6,23 +6,32 @@ import interfejsimenjacnice.Postavljanjekursa;
 
 public class Postavljanjekusra implements Postavljanjekursa {
 
+	private Kurs[] kursevi = new Kurs[10];
+	
 	@Override
 	public void dodavanjeKursaValuteZaOdredjeniDan(double prodajniKurs,
 			double srednjiKurs, double kupovniKurs, String nazivValute,
 			String skraceniNazivValute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		
+		kursevi[1].setKupovniKurs(kupovniKurs);
+		kursevi[1].setProdajniKurs(prodajniKurs);
+		kursevi[1].setSrednjiKurs(srednjiKurs);
+		
 	}
 
 	@Override
 	public void brisanjeKursaValuteZaOdredjeniDan(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		kursevi[1].setKupovniKurs(0);
 
 	}
 
 	@Override
 	public Kurs pronalazenjeKursaZaOdredjeniDan(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < kursevi.length; i++) {
+			kursevi[i].getKupovniKurs();
+		}
+		
 		return null;
 	}
 
